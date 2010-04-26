a636112e9aa76cd5e1b445661c8cf26624fe7cfa
Petrica Ghiurca <petrica@ghiurca.net>
2010-04-26 12:08:49 +0300

    finally figured out a way to display the current filtering and sorting state ... not the most elegant but it works.

--- public/javascripts/jqPersist.js
+++ public/javascripts/jqPersist.js
@@ -6,24 +6,52 @@
  * Licensed under the MIT License:
  * http://www.opensource.org/licenses/mit-license.php
  */
+Storage.prototype.setObject = function(key, value) {
+    this.setItem(key, JSON.stringify(value));
+}
+
+Storage.prototype.getObject = function(key) {
+    return JSON.parse(this.getItem(key));
+}
 
 var persistGridState = function(postData) {
-    if (localStorage) {
-        var key = 'jqGrid-' + this.id;
-        if (!this.p.pageLoaded) {
-            for (var i = 0; i < localStorage.length; i++) {
-                var localKey = localStorage.key(i);
-                if (localKey.substr(key)) {
-                    var paramKey = localKey.replace(key, '');
-                    postData[paramKey] = localStorage[localKey]
-                }
-            }
-            this.p.pageLoaded = true;
-        } else {
-            for (var e in postData) {
-                localStorage[key + e] = postData[e];
-            }
-        }
-    }
-    return postData;
+	if (localStorage) {
+		var key = 'gridParams:' + window.location.pathname + ":" + this.id;
+		if (!this.p.pageLoaded) {
+			$('span.ui-jqgrid-title', $('#gbox_'+this.id)).append('<span class="filtering"></span>');
+
+			if (localStorage.getItem(key)) {
+				postData = localStorage.getObject(key);
+				if (postData._search) {
+					colModel = this.p.colModel
+					filter = ""
+					filters_count = 0;
+					for (var col in colModel) {
+						var colName = colModel[col].name;
+						if (postData[colName]) {
+							if (filters_count > 0) filter += " and ";
+							filter += colName + ": " + postData[colName];
+							filters_count++;
+						}
+					}
+					$('span.ui-jqgrid-title span.filtering', $('#gbox_'+this.id)).append('<span class="state-indicator">Filtered on ' + filter + '</span>');
+				}
+
+				if (postData.sidx)
+					$('span.ui-jqgrid-title span.filtering', $('#gbox_'+this.id)).append('<span class="state-indicator">Sorted on ' + postData.sidx + '</span>');
+
+				if (postData.page > 1)
+					$('span.ui-jqgrid-title  span.filtering', $('#gbox_'+this.id)).append('<span class="state-indicator">Page ' + postData.page + '</span>');
+
+				var grid_id = this.id;
+				$('#refresh_' + this.id).live('mouseup', function() {
+					$('span.ui-jqgrid-title span.filtering', $('#gbox_'+grid_id)).empty();
+				});
+			}
+			this.p.pageLoaded = true;
+		} else {
+			localStorage.setObject(key, postData);
+		}
+	}
+  return postData;
 }
